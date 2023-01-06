e0903e1bb05ff0829bfd5df1109667f0a5cbee46
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-01-06 12:19:27 -0600

    complete models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The Engineer's Name can't be empty!")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "The Engineer's Details can't be empty!")]
     public string Details { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "The Machine's Name can't be empty!")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "The Machine's Details can't be empty!")]
     public string Details { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
